ded7550a86094d1ba51c64c54057a00d1a076ad9
Robert Stockton <59614662+BeardedTesterUK@users.noreply.github.com>
2022-02-16 12:56:02 +0000

    Add files via upload

---
+++ DataObs/src/main/java/ReadingData.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadingData {
+
+    public static List<String[]> readData() throws IOException {
+        int count = 0;
+        String file = "C:\\Users\\robert.stockton\\Desktop/CSVfile.csv";
+        List<String[]> content = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                content.add(line.split(","));
+            }
+        } catch (FileNotFoundException e) {
+        }
+        return content;
+    }
+}
