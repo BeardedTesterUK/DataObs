4cd52c6dd2e2a65b3d5a06f2a99e59a1c5a40963
robert.stockton <robert.stockton@hotmail.co.uk>
2022-02-17 14:49:48 +0000

    Date and Number Conversions added

--- DataObs/src/main/java/CSVRead.java
+++ DataObs/src/main/java/CSVRead.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,26 +11,37 @@ public class CSVRead {
     private static List<String[]> updatedContent;
 
     public static void main(String[] args) throws IOException {
-        originalData = getOriginalData();
-        List<String[]> authorisedData = cycleAndConvertData(originalData);
-        CreateFile.createFile(authorisedData);
+//        originalData = getOriginalData();
+//        List<String[]> authorisedData = cycleAndConvertStringData(originalData);
+//        CreateFile.createFile(authorisedData);
+
+
+//        ConvertingData.convertDateData();
+//        LocalDate future = ConvertingData.getFutureDate();
+//        LocalDate past = ConvertingData.getPastDate();
+//        System.out.println("Future date is " + future);
+//        System.out.println("Past date is " + past);
+
+        ConvertingData.convertNumericalData(100000);
     }
 
     public static List<String[]> getOriginalData() throws IOException {
             return originalData = ReadingData.readData();
         }
 
-    public static List<String[]> cycleAndConvertData(List<String[]> originalData) {
+    public static List<String[]> cycleAndConvertStringData(List<String[]> originalData) {
         updatedContent = new ArrayList<>();
             //outer loop;
-            for(int i = 0; i< originalData.size(); i++){//Set i to Ignore headings
+            for(int i = 0; i< originalData.size(); i++){
+                //if(i == 0){updatedContent.add(originalData.get(i));//Add Headings without conversion
+                    //i = i + 1;}
                 dataLine = originalData.get(i); // Contains  surname, first, job, phone, other
                 //Create new Array
                 convertedDataArray = new String[dataLine.length];
                 //Inner Loop
                 for(int j = 0; j< dataLine.length; j++){
                     String dataFieldContents = dataLine[j]; // Contains surname
-                    String convertedData = ConvertingData.generateRandomString(dataFieldContents); //Contains New Random Data
+                    String convertedData = ConvertingData.convertStringData(dataFieldContents); //Contains New Random Data
                     // convertedData added to new Array
                     convertedDataArray[j] = convertedDataArray[j] = convertedData;
                 } //End Inner Loop
--- DataObs/src/main/java/ConvertingData.java
+++ DataObs/src/main/java/ConvertingData.java
@@ -1,40 +1,115 @@
+import java.time.LocalDate;
 import java.util.Random;
-import java.util.Scanner;
 
 public class ConvertingData {
 
+    private static String asciiUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static String digits = "1234567890";
+    private static Random rand = new Random();
+    private static String asciiLowerCase = asciiUpperCase.toLowerCase();
+    private static String asciiChars = asciiUpperCase + asciiLowerCase + digits;
 
-        public static void main(String[] args) {
-            String asciiUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            String asciiLowerCase = asciiUpperCase.toLowerCase();
-            String digits = "1234567890";
-            String asciiChars = asciiUpperCase + asciiLowerCase + digits;
+//        public static void main(String[] args) {
+//            //String asciiUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//            //String asciiLowerCase = asciiUpperCase.toLowerCase();
+//            //String digits = "1234567890";
+//            //String asciiChars = asciiUpperCase + asciiLowerCase + digits;
+//        }
 
-            //String randomString = generateRandomString("Ro!BeR@t45!£^%&", asciiUpperCase, asciiLowerCase, digits);
-
-            //System.out.println(String.format("The random string is %s", randomString));
-        }
-
-        static String generateRandomString(String original) {
-            String asciiUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            String asciiLowerCase = asciiUpperCase.toLowerCase();
-            String digits = "1234567890";
+        static String convertStringData(String original) {
+            //String asciiUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            //String asciiLowerCase = asciiUpperCase.toLowerCase();
+            //String digits = "1234567890";
             StringBuilder sb = new StringBuilder();
             int i = 0;
-            Random rand = new Random();
+            //Random rand = new Random();
             while (i < original.length()) {
                 char x = original.charAt(i);
-                if (Character.isUpperCase(x)) {
-                    sb.append(asciiUpperCase.charAt(rand.nextInt(asciiUpperCase.length())));
-                } else if(Character.isLowerCase(x)){
-                    sb.append(asciiLowerCase.charAt(rand.nextInt(asciiLowerCase.length())));
-                }else if(Character.isDigit(x)){
-                    sb.append(digits.charAt(rand.nextInt(digits.length())));
+                if(asciiChars.contains(Character.toString(x))){
+                    if (Character.isUpperCase(x)) {
+                        //sb.append(asciiUpperCase.charAt(rand.nextInt(asciiUpperCase.length())));
+                        char y = changeUpperCase();
+                        sb.append(y);
+                    } else if(Character.isLowerCase(x)){
+                        //sb.append(asciiLowerCase.charAt(rand.nextInt(asciiLowerCase.length())));
+                        char y = changeLowerCase();
+                        sb.append(y);
+                    }else if(Character.isDigit(x)) {
+                        //if(x == '0'){sb.append(x);
+                        //}else{sb.append(digits.charAt(rand.nextInt(digits.length())));}
+                        char y = changeDigits(x);
+                        sb.append(y);
+                    }
                 }else {
                     sb.append(x);
                 }
                 i++;
             }
                 return sb.toString();
+        }
+
+        public static char changeUpperCase(){
+            return asciiUpperCase.charAt(rand.nextInt(asciiUpperCase.length()));
+        }
+
+        public static char changeLowerCase(){
+            String asciiLowerCase = asciiUpperCase.toLowerCase();
+            return asciiLowerCase.charAt(rand.nextInt(asciiLowerCase.length()));
+        }
+
+        public static char changeDigits(char x){
+            if(x == '0'){
+                return (x);
+            }else {
+                return digits.charAt(rand.nextInt(digits.length()));}
+        }
+
+        public static void convertDateData(){
+            int minDay = (int) LocalDate.of(1900, 1, 1).toEpochDay();
+            int maxDay = (int) LocalDate.of(2020, 1, 1).toEpochDay();
+            long randomDay = minDay + rand.nextInt(maxDay - minDay);
+
+            LocalDate randomBirthDate = LocalDate.ofEpochDay(randomDay);
+            LocalDate today = LocalDate.now();
+            System.out.println(randomBirthDate + " " + today);
+
+        }
+
+        public static LocalDate getPastDate(){
+            int minDay = (int) LocalDate.of(1900, 1, 1).toEpochDay();
+            int maxDay = (int) LocalDate.now().toEpochDay();
+            long randomDay = minDay + rand.nextInt(maxDay - minDay);
+
+            LocalDate randomBirthDate = LocalDate.ofEpochDay(randomDay);
+            return randomBirthDate;
+        }
+
+        public static LocalDate getFutureDate(){
+            int minDay = (int) LocalDate.now().toEpochDay();
+            int maxDay = (int) LocalDate.of(2100, 1, 1).toEpochDay();
+            long randomDay = minDay + rand.nextInt(maxDay - minDay);
+
+            LocalDate randomBirthDate = LocalDate.ofEpochDay(randomDay);
+            return randomBirthDate;
+        }
+
+        public static int convertNumericalData(int amount){ //Salary
+            double difference = (amount * 0.1);
+            int minAmount = (int) (amount - difference);
+            int maxAmount = (int) (amount + difference);
+            int range = maxAmount - minAmount;
+
+            int randomNumber = minAmount + rand.nextInt(range);
+            System.out.println("Amount: " + amount);
+            System.out.println("Min: " + minAmount);
+            System.out.println("Max " + maxAmount);
+            System.out.println("Range: " + range);
+            System.out.println("Random No: " + randomNumber);
+
+            return randomNumber;
+        }
+
+        public static void convertIDData(){
+
         }
 }
